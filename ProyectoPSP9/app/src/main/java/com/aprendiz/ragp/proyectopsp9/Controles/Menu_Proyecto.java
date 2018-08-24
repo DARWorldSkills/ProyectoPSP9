8d5c339fc86798785bb1c8535e5f73017bd4a72c
DavidPyn27 <popayan159@gmail.com>
2018-08-24 09:29:21 -0500

    menu_proyecto

--- ProyectoPSP9/app/src/main/java/com/aprendiz/ragp/proyectopsp9/Controles/Menu_Proyecto.java
+++ ProyectoPSP9/app/src/main/java/com/aprendiz/ragp/proyectopsp9/Controles/Menu_Proyecto.java
@@ -1,15 +1,72 @@
+
 package com.aprendiz.ragp.proyectopsp9.Controles;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.aprendiz.ragp.proyectopsp9.R;
 
-public class Menu_Proyecto extends AppCompatActivity {
+public class Menu_Proyecto extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnTimer, btnDefect, btnPlanSummary;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu__proyecto);
+
+        inicializar();
+        escuchar();
+
+    }
+
+    //Hacemos escuchar el boton para que este pueda funcionar
+    private void escuchar() {
+        btnTimer.setOnClickListener(this);
+        btnDefect.setOnClickListener(this);
+        btnPlanSummary.setOnClickListener(this);
+
+    }
+
+    //Referenciamos Los campos
+    private void inicializar() {
+
+        btnTimer = findViewById(R.id.btnTimerLog);
+        btnDefect = findViewById(R.id.btnDefectLog);
+        btnPlanSummary = findViewById(R.id.btnProyectPlanS);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.btnTimerLog:
+
+                //Funcion  que permite ir de una actividad a otra
+                Intent intent = new Intent(Menu_Proyecto.this,TimerLog.class);
+                startActivity(intent);
+
+                break;
+
+            case R.id.btnDefectLog:
+
+                Intent intent1 = new Intent(Menu_Proyecto.this,DefectLog.class);
+                startActivity(intent1);
+
+                break;
+
+            case R.id.btnProyectPlanS:
+                Intent intent2 = new Intent(Menu_Proyecto.this,ProyectPlanSummary.class);
+                startActivity(intent2);
+
+                break;
+
+        }
+
     }
 }
