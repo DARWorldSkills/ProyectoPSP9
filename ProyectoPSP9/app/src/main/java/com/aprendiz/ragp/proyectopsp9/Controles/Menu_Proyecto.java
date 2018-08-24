410f7ed726f18c2f4b44667708de0f9d8054ed70
DavidPyn27 <popayan159@gmail.com>
2018-08-24 08:59:14 -0500

    Layout de psp

---
+++ ProyectoPSP9/app/src/main/java/com/aprendiz/ragp/proyectopsp9/Controles/Menu_Proyecto.java
@@ -0,0 +1,15 @@
+package com.aprendiz.ragp.proyectopsp9.Controles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aprendiz.ragp.proyectopsp9.R;
+
+public class Menu_Proyecto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu__proyecto);
+    }
+}
