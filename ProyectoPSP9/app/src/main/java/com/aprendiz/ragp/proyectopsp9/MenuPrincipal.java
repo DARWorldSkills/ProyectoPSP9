2b03b5fbc80c9ffcf74ee50eba2c54a2ada341f2
Rodrigo Alejandro Guspian Perez <raguspian@misena.edu.co>
2018-08-24 08:35:30 -0500

    first commit V

--- ProyectoPSP9/app/src/main/java/com/aprendiz/ragp/proyectopsp9/MenuPrincipal.java
+++ ProyectoPSP9/app/src/main/java/com/aprendiz/ragp/proyectopsp9/MenuPrincipal.java
@@ -3,11 +3,11 @@ package com.aprendiz.ragp.proyectopsp9;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Splash extends AppCompatActivity {
+public class MenuPrincipal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_menu_principal);
     }
 }
